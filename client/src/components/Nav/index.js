dad928e288d2de9c054b93a1244dc8cfb04df9cf
Miles Standoffish <toddnan@gmail.com>
2019-10-26 19:20:48 -0500

    navbar collapse working

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,27 +1,45 @@
-import React from "react";
+// https://www.bennettnotes.com/bootstrap-navbar-collapse-reactjs/
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import "./style.css";
 
-function Nav( { activePage } ) {
-  return (
-    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-      <a className="navbar-brand" href="/">
-        Booklooker!
-      </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className={ activePage === "search" ? "nav-item active" : "nav-item" } >
-            <a className="nav-link" href="/search">Search <span className="sr-only">(current)</span></a>
-          </li>
-          <li className={ activePage === "saved" ? "nav-item active" : "nav-item" }>
-            <a className="nav-link" href="/saved">Saved</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true,
+    };
+  }
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render() {
+    const collapsed = this.state.collapsed;
+    const classOne = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
+    const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark transparent-nav">
+        <div className="container">
+          <a className="navbar-brand" href="#">Booklooker</a>
+          <button onClick={this.toggleNavbar} className={`${classTwo}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className={`${classOne}`} id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <li className={ this.props.activePage === "search" ? "nav-item active" : "nav-item" }>
+                <Link className="nav-link" to="/search">Search</Link>
+              </li>
+              <li className={ this.props.activePage === "saved" ? "nav-item active" : "nav-item" }>
+                <Link className="nav-link" to="/saved">Saved</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
-
 export default Nav;
